3785f5e5576852172c7a5b697b8bec5500312722
Nicola Toscan <nicolatoscan99@outlook.it>
2020-06-17 22:20:15 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,82 @@
+const puppeteer = require('puppeteer');
+const { Telegraf } = require('telegraf')
+
+require('dotenv').config();
+
+let finito = false;
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+async function doUrl(url) {
+    const browser = await puppeteer.launch({ headless: false })
+    const page = await browser.newPage();
+
+    while (true) {
+        if (finito) {
+            browser.close()
+            return;
+        }
+
+
+        await page.goto(url);
+
+        //LOGIN
+        if (page.url().indexOf("idp.unitn.it/idp/profile/SAML2") >= 0) {
+            let iid = Math.random()
+            sendMessage("LOGGANDO" + iid);
+
+            await sleep(400);
+            page.type("input#clid.form-control", 'nicola.toscan');
+            await sleep(400);
+            page.type("input#inputPassword", process.env.PASSWORD);
+            await sleep(400);
+            page.click("#btnAccedi")
+            await sleep(400);
+
+            while (page.url().indexOf("esse3.unitn.it/auth/studente/Appelli") < 0) {
+                await sleep(300);
+                await page.waitForNavigation({ waitUntil: 'networkidle0' })
+            }
+            sendMessage("LOGGATO" + iid);
+        }
+
+
+        //CLICCA
+        page.click("#btnSalva");
+        while (page.url().indexOf("esse3.unitn.it/auth/studente/Appelli/EffettuaPrenotazioneAppello") < 0) {
+            await sleep(300);
+            await page.waitForNavigation({ waitUntil: 'networkidle0' })
+        }
+
+        const element = await page.$("#app-text_esito_pren_msg");
+        const text = await page.evaluate(element => element.textContent, element);
+        if (text.indexOf("PRENOTAZIONE NON EFFETTUATA") < 0) {
+            sendMessage("FATTO");
+            finito = true;
+        }
+        lastTest[url] = new Date();
+        await sleep(2000);
+
+    }
+
+};
+
+
+function sleep(d) {
+    return new Promise(r => setTimeout(r, d));
+}
+
+function sendMessage(text) {
+    try {
+        bot.telegram.sendMessage(process.env.CHAT_ID, text);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+bot.on("message", (ctx) => ctx.reply("Last test:\n" + Object.keys(lastTest).map(k => lastTest[k]).join("\n")))
+bot.launch();
+
+(async () => {
+    let esami = process.env.ESAMI_URLS.split(",");
+    esami.forEach(e => doUrl(e));
+})();
